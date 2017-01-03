fa7954907622ef35f945eb07284dc6f7f0474a31
stephenhealy <healy@sitesteam.com>
2017-01-03 15:13:03 -0500

    test

--- VideoBooth/frmMain.Designer.cs
+++ VideoBooth/frmMain.Designer.cs
@@ -47,9 +47,10 @@
             this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStop.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStop.ForeColor = System.Drawing.Color.White;
-            this.btnStop.Location = new System.Drawing.Point(160, 242);
+            this.btnStop.Location = new System.Drawing.Point(213, 298);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(250, 40);
+            this.btnStop.Size = new System.Drawing.Size(333, 49);
             this.btnStop.TabIndex = 34;
             this.btnStop.Text = "Stop Event";
             this.btnStop.UseVisualStyleBackColor = false;
@@ -60,28 +61,31 @@
             // 
             this.ddlEvent.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ddlEvent.FormattingEnabled = true;
-            this.ddlEvent.Location = new System.Drawing.Point(160, 137);
+            this.ddlEvent.Location = new System.Drawing.Point(213, 169);
+            this.ddlEvent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ddlEvent.Name = "ddlEvent";
-            this.ddlEvent.Size = new System.Drawing.Size(250, 26);
+            this.ddlEvent.Size = new System.Drawing.Size(332, 31);
             this.ddlEvent.TabIndex = 32;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(95, 140);
+            this.label5.Location = new System.Drawing.Point(60, 172);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 18);
+            this.label5.Size = new System.Drawing.Size(132, 23);
             this.label5.TabIndex = 31;
-            this.label5.Text = "Event :";
+            this.label5.Text = "Select Event :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(156, 64);
+            this.label4.Location = new System.Drawing.Point(208, 79);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 18);
+            this.label4.Size = new System.Drawing.Size(117, 23);
             this.label4.TabIndex = 27;
             this.label4.Text = "125,098 GB";
             // 
@@ -89,9 +93,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(68, 94);
+            this.label3.Location = new System.Drawing.Point(91, 116);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 18);
+            this.label3.Size = new System.Drawing.Size(109, 23);
             this.label3.TabIndex = 26;
             this.label3.Text = "Last Sync :";
             // 
@@ -99,9 +104,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(156, 94);
+            this.label2.Location = new System.Drawing.Point(208, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 18);
+            this.label2.Size = new System.Drawing.Size(110, 23);
             this.label2.TabIndex = 25;
             this.label2.Text = "12/21/2016";
             // 
@@ -109,9 +115,10 @@
             // 
             this.lblHeader.AutoSize = true;
             this.lblHeader.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeader.Location = new System.Drawing.Point(12, 21);
+            this.lblHeader.Location = new System.Drawing.Point(16, 26);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(286, 22);
+            this.lblHeader.Size = new System.Drawing.Size(359, 29);
             this.lblHeader.TabIndex = 24;
             this.lblHeader.Text = "Welcome back to Video Booth";
             // 
@@ -119,9 +126,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 64);
+            this.label1.Location = new System.Drawing.Point(17, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 18);
+            this.label1.Size = new System.Drawing.Size(175, 23);
             this.label1.TabIndex = 23;
             this.label1.Text = "Space Remaining :";
             // 
@@ -131,9 +139,10 @@
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStart.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart.ForeColor = System.Drawing.Color.Green;
-            this.btnStart.Location = new System.Drawing.Point(160, 196);
+            this.btnStart.Location = new System.Drawing.Point(213, 241);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(250, 40);
+            this.btnStart.Size = new System.Drawing.Size(333, 49);
             this.btnStart.TabIndex = 22;
             this.btnStart.Text = "Start Event";
             this.btnStart.UseVisualStyleBackColor = false;
@@ -145,9 +154,10 @@
             this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogout.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.ForeColor = System.Drawing.Color.Black;
-            this.btnLogout.Location = new System.Drawing.Point(439, 13);
+            this.btnLogout.Location = new System.Drawing.Point(585, 16);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(100, 40);
+            this.btnLogout.Size = new System.Drawing.Size(133, 49);
             this.btnLogout.TabIndex = 21;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
@@ -157,17 +167,18 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(156, 169);
+            this.label6.Location = new System.Drawing.Point(208, 208);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(393, 15);
+            this.label6.Size = new System.Drawing.Size(465, 17);
             this.label6.TabIndex = 35;
             this.label6.Text = "In the future, this should be changed to a file browse to import the data.";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 304);
+            this.ClientSize = new System.Drawing.Size(735, 374);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.ddlEvent);
@@ -179,6 +190,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnLogout);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.Text = "Video Booth";
             this.ResumeLayout(false);
